dfe0462ea707dfd77987487eec58f4fb652ed378
nxrmqlly <ritam.das3110@outlook.com>
2023-12-07 20:37:28 +0530

    update LICENCE to agpl v3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Repository:
 __copyright__ = "Copyright (C) 2023 Ritam Das"
 __title__ = "Orbyt"
 __author__ = "Nxrmqlly (Ritam Das)"
-__license__ = "GPL v3"
+__license__ = "AGPL v3"
 
 import os
 from asyncio import run
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 888      888 oooo d8b  888oooo.  oooo    ooo .o888oo      By Nxrmqlly
 888      888 `888""8P  d88' `88b  `88.  .8'    888        https://github.com/nxrmqlly/Orbyt
 888      888  888      888   888   `88..8'     888
-`88b    d88'  888      888   888    `888'      888 .      Copyright (C) 2023 Ritam Das | GPL v3
+`88b    d88'  888      888   888    `888'      888 .      Copyright (C) 2023 Ritam Das | AGPL v3
  `Y8bood8P'  d888b     `Y8bod8P'     .8'       "888" 
                                  .o..P'              
                                  `Y8P'               
